65d3cda8742e66ea7774110c6ca7e0673deb3dae
cassandro-pereira <112014056+cassandro-pereira@users.noreply.github.com>
2024-03-27 16:06:33 -0300

    Update lotery.py

--- lotery.py
+++ lotery.py
@@ -12,19 +12,15 @@ while x <= 60 :
 print("All numbers")
 print(alllist)
 
-def sortnumber(list,time):
-    for x in list:
-        if x < 30:              
-            return (time%60 * x)
-        else:
-            return (time%60 / x)           
-     
+def sortnumber(time):             
+    return time%60             
+    
 x = 0
 while x <= 6:
     timestamp = int(datetime.datetime.now().strftime("%f"))        
 #    print(timestamp)
     time.sleep(.1)
-    number = sortnumber(alllist,timestamp)
+    number = sortnumber(timestamp)
     lucklist.append(number)
     x += 1
 print("These are your 6 lucky numbers")
